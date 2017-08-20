87f74d24e4d66224555dff6de1a08983f84bf2a8
Oliverlyc <82738320@qq.com>
2017-08-20 18:23:40 +0800

    更新

--- 函数.php
+++ 函数.php
@@ -1,3 +1,11 @@
+1.伪类型mixed,number,callback
+2.函数形参中如果带有"&",在函数调用时必须传入一个变量
+3.默认参数'[]'必须在没有默认参数的后面
+<?php 
+	func_get_args(oid)//返回一个数组，包含所有参数
+	func_num_args(oid)//返回参数总数
+	func_get_arg(arg_num)//接收一个数字参数,返回指定参数
+ ?>
 <?php 
  
 	var_dump($expression)//显示结构信息，包括类型和值，数组会递归展开式输出
--- 变量定义.php
+++ 变量定义.php
@@ -1,3 +1,6 @@
+1.函数内部变量以及参数都是局部变量
+2.调用局部变量必须使用renturn返回
+3.静态变量不随着函数结束而释放,在所有对该函数之间共享
 <?php
 	.=//连续定义变量
 	//局部作用域，全局作用域，静态作用域，函数参数
@@ -6,10 +9,9 @@
 	}
 	$counter = 10;
 	updateCounter();
-	echo $counter;
-	//函数里的$counter只是局部变量 全局变量$counter还是10
-    global $counter
-    //全局变量$counter
+	echo $counter;//函数里的$counter只是局部变量 全局变量$counter还是10
+    
+    global $counter//全局变量$counter,在函数中使用全局变量必须要用global
 
     $a = 0b110001010 //0b开头作为二进制
 
